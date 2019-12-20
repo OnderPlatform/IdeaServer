cd4194df77d933137157e80cb14e1abae77d0637
Kyzyl-ool <kyzyloolk@mail.ru>
2019-12-20 14:42:14 +0300

    test rerunning

--- src/Application.ts
+++ src/Application.ts
@@ -45,7 +45,7 @@ export default class Application {
     this.db.service.mqtt.mqtt_cl.add_handler((value: string, message: string) => this.db.service.mqtt.newTransactionStateFromMQTT(value, message))
     this.db.service.mqtt.mqtt_cl.start()
 
-    console.log("post data cron")
+    console.log("post data cron on start")
     this.postData()
     // this.fetchingData()
 
